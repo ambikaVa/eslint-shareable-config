678e71da2c7662f3e2127959ee09470ac1b87275
ambika <ambika.vaish04@gmail.com>
2025-07-07 18:50:21 +0530

    add shareable config file

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+
+export default ([
+  { 
+    
+     languageOptions: {
+      ecmaVersion: 'latest',  // Enables modern JS
+      sourceType: 'module',   // Enables import/export,
+    },
+    rules:{
+     "consistent-return": "error",
+"no-console": "error",
+"no-restricted-imports":["error","underscore"],
+"eqeqeq":"error",
+  } }
+]);
+
+
